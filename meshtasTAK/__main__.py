2ffab69250ff690672128368fe70e3c679598f75
Delta Bravo One Five <deltabravo15ga@gmail.com>
2020-08-11 15:53:42 -0400

    Update __main__.py

--- meshtasTAK/__main__.py
+++ meshtasTAK/__main__.py
@@ -21,12 +21,15 @@ from pubsub import pub
 
 sleeptime = 0.075
 
-TAK_IP = sys.argv[1]
-TAK_PORT = sys.argv[2]
+if sys.argv[1]:
+	TAK_IP = sys.argv[1]
+else:
+	TAK_IP = 'localhost'
 
-#TAK_IP = '192.168.10.130'
-#TAK_IP = '204.48.30.216'
-#TAK_PORT = 8087
+if sys.arg[2]:
+	TAK_PORT = sys.argv[2]
+else:
+	TAK_PORT = 8087
 
 #TAK_IP = os.getenv('ATAK_IP', '204.48.30.216')
 #TAK_PORT = int(os.getenv('ATAK_PORT', '8087'))
