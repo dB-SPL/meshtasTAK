da4a544c5c9a7843c6e2539cdb8aa89907f10da8
Delta Bravo One Five <deltabravo15ga@gmail.com>
2020-08-11 14:05:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
       long_description = fh.read()
 
 setup(name='meshtasTAK',
-      version='0.1.1',
+      version='0.1.2',
       description='TAK Server and CoT Library',
       url='http://github.com/DeltaBravo15/meshtasTAK',
       author='DeltaBravo15',
