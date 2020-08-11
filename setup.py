b3ba5807d3183beb4cbac603ef0c62ef7fb422c1
Delta Bravo One Five <deltabravo15ga@gmail.com>
2020-08-11 11:16:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='meshtasTAK',
       url='http://github.com/DeltaBravo15/meshtasTAK',
       author='DeltaBravo15',
       license='GPLv3+',
-      packages=['meshtasTAK'],
+      packages=setuptools.find_packages(),
       zip_safe=True,
       python_requires='>=3.6',
       classifiers=[
